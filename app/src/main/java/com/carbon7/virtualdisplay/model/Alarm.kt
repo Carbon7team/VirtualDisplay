292b9d232f8c9d63d1821f0d81b668857334f401
Leonardo Speranzon <leonardo.speranzon@gmail.com>
2022-04-01 19:19:32 +0200

    Rimozione attributo isActive dagli allarmi

--- app/src/main/java/com/carbon7/virtualdisplay/model/Alarm.kt
+++ app/src/main/java/com/carbon7/virtualdisplay/model/Alarm.kt
@@ -1,7 +1,7 @@
 package com.carbon7.virtualdisplay.model
 
 
-data class Alarm(val code:String, val name:String, val level:Level, var isActive:Boolean = false){
+data class Alarm(val code:String, val name:String, val level:Level/*, var isActive:Boolean = false*/){
     enum class Level{
         NONE,WARNING,CRITICAL
     }
