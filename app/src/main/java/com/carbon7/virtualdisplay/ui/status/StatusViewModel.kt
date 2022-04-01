69fb765d4c98291f190f4af9a55bf8d69864f302
Leonardo Speranzon <leonardo.speranzon@gmail.com>
2022-04-01 20:33:50 +0200

    Creazione layout base

--- app/src/main/java/com/carbon7/virtualdisplay/ui/status/StatusViewModel.kt
+++ app/src/main/java/com/carbon7/virtualdisplay/ui/status/StatusViewModel.kt
@@ -1,7 +1,19 @@
 package com.carbon7.virtualdisplay.ui.status
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.carbon7.virtualdisplay.model.Status
 
 class StatusViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+
+    private var status : LiveData<List<Status>> = MutableLiveData(listOf(
+        Status("A001","Prova",true),
+        Status("A002","Bla Bla",true),
+        Status("A003","lalalalalal",false)
+    ))
+
+
+
+// TODO: Implement the ViewModel
 }
