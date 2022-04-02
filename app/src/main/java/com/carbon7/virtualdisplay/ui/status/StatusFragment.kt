f976abe7528115236863188d0465e3df9862dfab
Leonardo Speranzon <leonardo.speranzon@gmail.com>
2022-04-02 14:55:54 +0200

    Implementazione lista degli stati che si aggiorna

--- app/src/main/java/com/carbon7/virtualdisplay/ui/status/StatusFragment.kt
+++ app/src/main/java/com/carbon7/virtualdisplay/ui/status/StatusFragment.kt
@@ -2,12 +2,15 @@ package com.carbon7.virtualdisplay.ui.status
 
 import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
 import com.carbon7.virtualdisplay.R
 import com.carbon7.virtualdisplay.databinding.FragmentStatusBinding
+import com.carbon7.virtualdisplay.model.Status
 
 class StatusFragment : Fragment() {
 
@@ -30,8 +33,25 @@ class StatusFragment : Fragment() {
 
         viewModel = ViewModelProvider(this).get(StatusViewModel::class.java)
 
+        viewModel.status.observe(viewLifecycleOwner,{
+            Log.d("MyApp","UPDATE")
+            binding.listStatus.adapter = StatusAdapter(requireContext(),it)
+        })
+
+
+
+
         _binding = FragmentStatusBinding.inflate(inflater, container, false)
         val root: View = binding.root
+
+        binding.btn1.setOnClickListener { viewModel.addS000(true)}
+        binding.btn2.setOnClickListener { viewModel.addS001(true)}
+        binding.btn3.setOnClickListener { viewModel.addS002(true)}
+        binding.btn4.setOnClickListener { viewModel.addS000(false)}
+        binding.btn5.setOnClickListener { viewModel.addS001(false)}
+        binding.btn6.setOnClickListener { viewModel.addS002(false)}
+
+
 
         return root
     }
--- app/src/main/java/com/carbon7/virtualdisplay/ui/status/StatusViewModel.kt
+++ app/src/main/java/com/carbon7/virtualdisplay/ui/status/StatusViewModel.kt
@@ -1,18 +1,39 @@
 package com.carbon7.virtualdisplay.ui.status
 
+import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.carbon7.virtualdisplay.R
 import com.carbon7.virtualdisplay.model.Status
 
 class StatusViewModel : ViewModel() {
 
-    private var status : LiveData<List<Status>> = MutableLiveData(listOf(
-        Status("A001","Prova",true),
-        Status("A002","Bla Bla",true),
-        Status("A003","lalalalalal",false)
-    ))
+    private val _status = MutableLiveData(
+        listOf(
+            Status("S000", R.string.S000,true),
+            Status("S001",R.string.S001,true),
+            Status("S002",R.string.S002,false)
+        )
+    )
+    var status : LiveData<List<Status>> = _status
 
+
+    fun addS000(b:Boolean){
+        val l =_status.value!!.toMutableList()
+        l.add(Status("S000", R.string.S000,b))
+        _status.value= l
+    }
+    fun addS001(b:Boolean){
+        val l =_status.value!!.toMutableList()
+        l.add(Status("S001", R.string.S001,b))
+        _status.value= l
+    }
+    fun addS002(b:Boolean){
+        val l =_status.value!!.toMutableList()
+        l.add(Status("S002", R.string.S002,b))
+        _status.value= l
+    }
 
 
 // TODO: Implement the ViewModel
