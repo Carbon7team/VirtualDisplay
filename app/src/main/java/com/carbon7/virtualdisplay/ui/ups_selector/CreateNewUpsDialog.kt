7a0b57d1676769f6b1cc24f4f21dce8eba8f91f4
iamadnangazi <adnangazi.ag@gmail.com>
2022-05-08 17:48:33 +0200

    Error management

--- app/src/main/java/com/carbon7/virtualdisplay/ui/ups_selector/CreateNewUpsDialog.kt
+++ app/src/main/java/com/carbon7/virtualdisplay/ui/ups_selector/CreateNewUpsDialog.kt
@@ -5,58 +5,75 @@ import android.app.Dialog
 import android.content.DialogInterface
 import android.os.Bundle
 import android.view.LayoutInflater
-import android.widget.Toast
 import androidx.fragment.app.DialogFragment
 import com.carbon7.virtualdisplay.R
 import com.carbon7.virtualdisplay.databinding.DialogCreateUpsBinding
-import java.io.IOException
 import java.util.regex.Pattern
 
-class CreateNewUpsDialog(private val onSuccess:(name: String, ip: String, port: Int)->Unit) :
+class CreateNewUpsDialog(private val onSuccess: (name: String, ip: String, port: Int) -> Unit) :
     DialogFragment() {
 
-    var zeroTo255 = ("(\\d{1,2}|(0|1)\\"
+    private var _binding: DialogCreateUpsBinding? = null
+    private val binding
+        get() = _binding!!
+
+    private var zeroTo255 = ("(\\d{1,2}|(0|1)\\"
             + "d{2}|2[0-4]\\d|25[0-5])")
 
-    var p: Pattern = Pattern.compile(
-            (zeroTo255 + "\\."
-            + zeroTo255 + "\\."
-            + zeroTo255 + "\\."
-            + zeroTo255)
+    private var p: Pattern = Pattern.compile(
+        (zeroTo255 + "\\."
+                + zeroTo255 + "\\."
+                + zeroTo255 + "\\."
+                + zeroTo255)
     )
 
-    override fun onCreateDialog(savedInstanceState: Bundle?) : Dialog {
+    override fun onResume() {
+        super.onResume()
+        (dialog as AlertDialog).getButton(DialogInterface.BUTTON_POSITIVE).setOnClickListener {
+            // value format check
+            var correctInput = true
+
+            val name = binding.upsName.text.toString()
+            if (name == "") {
+                correctInput = false
+                binding.upsName.error = "UPS NAME CAN NOT BE EMPTY!"
+            }
+
+            val ip = binding.upsIp.text.toString()
+            if (!p.matcher(ip).matches()) {
+                correctInput = false
+                binding.upsIp.error = "IP ADDRESS WRONG FORMAT!"
+            }
+
+            try {
+               val port = binding.upsPort.text.toString().toInt()
+                if (port < 0 || port > 65535) {
+                    correctInput = false
+                    binding.upsPort.error = "UPS NAME CAN NOT BE EMPTY!"
+                }
+                if (correctInput) {
+                    dismiss()
+                    onSuccess(name, ip, port)
+                }
+
+            } catch (e: NumberFormatException) {
+                binding.upsPort.error = "UPS NAME CAN NOT BE EMPTY!"
+            }
+        }
+    }
+
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
         return activity?.let {
             val builder = AlertDialog.Builder(it)
-            val binding = DialogCreateUpsBinding.inflate(LayoutInflater.from(context))
+            _binding = DialogCreateUpsBinding.inflate(LayoutInflater.from(context))
 
             builder.setTitle(R.string.add_new_ups)
                 .setView(binding.root)
-                .setPositiveButton(R.string.add,
-                    DialogInterface.OnClickListener { dialog, id ->
-                        // value format check
-                        try {
-                            val name = binding.upsName.text.toString()
-                            val ip = binding.upsIp.text.toString()
-                            val port = binding.upsPort.text.toString().toInt()
-
-                            if (name == "") throw IOException("UPS NAME CAN NOT BE EMTY!")
-                            if (!p.matcher(ip).matches()) throw IOException("IP ADDRESS FORMAT IS NOT CORRECT!")
-                            else if (port < 0 || port > 65535) throw IOException("PORT NUMBER VALUE IS NOT CORRECT!")
-                            else {
-                                Toast.makeText(context, "UPS ADDED", Toast.LENGTH_SHORT).show()
-                                onSuccess(name, ip, port)
-                            }
-
-                        } catch (e: IOException) {
-                            Toast.makeText(context, e.message, Toast.LENGTH_SHORT).show()
-                        } catch (e: Exception) {
-                            Toast.makeText(context, "WRONG FORMAT OF VALUES!", Toast.LENGTH_SHORT).show()
-                        }
-                    })
+                .setPositiveButton(R.string.add, null)
                 .setNegativeButton(R.string.cancel, null)
 
             builder.create()
+
         } ?: throw IllegalStateException("Activity cannot be null")
     }
 }
--- app/src/main/java/com/carbon7/virtualdisplay/ui/ups_selector/ModifyUpsDialog.kt
+++ app/src/main/java/com/carbon7/virtualdisplay/ui/ups_selector/ModifyUpsDialog.kt
@@ -15,6 +15,10 @@ import java.util.regex.Pattern
 class ModifyUpsDialog(private var name: String, private val ip: String, private val port: Int, private val onSuccess: (name: String, ip: String, port: Int) -> Unit) :
     DialogFragment() {
 
+    private var _binding: DialogCreateUpsBinding? = null
+    private val binding
+        get() = _binding!!
+
     private var zeroTo255 = ("(\\d{1,2}|(0|1)\\"
             + "d{2}|2[0-4]\\d|25[0-5])")
 
@@ -24,6 +28,31 @@ class ModifyUpsDialog(private var name: String, private val ip: String, private 
                 + zeroTo255 + "\\."
                 + zeroTo255)
     )
+
+    override fun onResume() {
+        super.onResume()
+        (dialog as AlertDialog).getButton(DialogInterface.BUTTON_POSITIVE).setOnClickListener {
+            // value format check
+            try {
+                val name = binding.upsName.text.toString()
+                val ip = binding.upsIp.text.toString()
+                val port = binding.upsPort.text.toString().toInt()
+
+                if (name == "") throw IOException("UPS NAME CAN NOT BE EMTY!")
+                if (!p.matcher(ip).matches()) throw IOException("IP ADDRESS FORMAT IS NOT CORRECT!")
+                else if (port < 0 || port > 65535) throw IOException("PORT NUMBER VALUE IS NOT CORRECT!")
+                else {
+                    Toast.makeText(context, "UPS ADDED", Toast.LENGTH_SHORT).show()
+                    onSuccess(name, ip, port)
+                }
+
+            } catch (e: IOException) {
+                Toast.makeText(context, e.message, Toast.LENGTH_SHORT).show()
+            } catch (e: Exception) {
+                Toast.makeText(context, "WRONG FORMAT OF VALUES!", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
 
     override fun onCreateDialog(savedInstanceState: Bundle?) : Dialog {
         return activity?.let {
@@ -36,28 +65,7 @@ class ModifyUpsDialog(private var name: String, private val ip: String, private 
 
             builder.setTitle(R.string.modify_this_ups)
                 .setView(binding.root)
-                .setPositiveButton(R.string.save,
-                    DialogInterface.OnClickListener { dialog, id ->
-                        // value format check
-                        try {
-                            val name = binding.upsName.text.toString()
-                            val ip = binding.upsIp.text.toString()
-                            val port = binding.upsPort.text.toString().toInt()
-
-                            if (name == "") throw IOException("UPS NAME CAN NOT BE EMTY!")
-                            if (!p.matcher(ip).matches()) throw IOException("IP ADDRESS FORMAT IS NOT CORRECT!")
-                            else if (port < 0 || port > 65535) throw IOException("PORT NUMBER VALUE IS NOT CORRECT!")
-                            else {
-                                Toast.makeText(context, "UPS MODIFIED", Toast.LENGTH_SHORT).show()
-                                onSuccess(name, ip, port)
-                            }
-
-                        } catch (e: IOException) {
-                            Toast.makeText(context, e.message, Toast.LENGTH_SHORT).show()
-                        } catch (e: Exception) {
-                            Toast.makeText(context, "WRONG FORMAT OF VALUES!", Toast.LENGTH_SHORT).show()
-                        }
-                    })
+                .setPositiveButton(R.string.save, null)
                 .setNegativeButton(R.string.cancel, null)
 
             builder.create()
