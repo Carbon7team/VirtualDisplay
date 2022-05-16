f2c51c1c52ea2ab3e38b475e08bced6a7043e66c
iamadnangazi <adnangazi.ag@gmail.com>
2022-05-16 11:09:56 +0200

    Bypass fragment

--- app/src/main/java/com/carbon7/virtualdisplay/ui/bypass/BypassFragment.kt
+++ app/src/main/java/com/carbon7/virtualdisplay/ui/bypass/BypassFragment.kt
@@ -4,33 +4,84 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
+import androidx.fragment.app.viewModels
+import com.carbon7.virtualdisplay.R
+import com.carbon7.virtualdisplay.databinding.FragmentAlarmsBinding
 import com.carbon7.virtualdisplay.databinding.FragmentBypassBinding
+import com.carbon7.virtualdisplay.ui.UpsDataVisualizerFragment
 
-class BypassFragment : Fragment() {
+class BypassFragment : UpsDataVisualizerFragment() {
+    override val viewModel: BypassViewModel by viewModels()
 
-    companion object {
-        fun newInstance() = BypassFragment()
-    }
-
-    private lateinit var viewModel: BypassViewModel
     private var _binding: FragmentBypassBinding? = null
-
-    // This property is only valid between onCreateView and
-    // onDestroyView.
     private val binding
         get() = _binding!!
+
+    private val rotateOpen: Animation by lazy { AnimationUtils.loadAnimation(context, R.anim.rotate_open_anim)}
+    private val rotateClose: Animation by lazy { AnimationUtils.loadAnimation(context,R.anim.rotate_close_anim)}
+    private val fromBottom: Animation by lazy { AnimationUtils.loadAnimation(context,R.anim.from_bottom_anim)}
+    private val toBottom: Animation by lazy { AnimationUtils.loadAnimation(context,R.anim.to_botton_anim)}
+    private var fabOpened : Boolean = false;
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        viewModel = ViewModelProvider(this).get(BypassViewModel::class.java)
-
         _binding = FragmentBypassBinding.inflate(inflater, container, false)
-        val root: View = binding.root
 
-        return root
+        setupView()
+        setupVM()
+
+        return binding.root
+    }
+
+    private fun setupVM(){
+        binding.listAlarms.adapter = AlarmAdapter(viewModel.filteredAlarms.value ?: listOf())
+        viewModel.filteredAlarms.observe(viewLifecycleOwner){
+            (binding.listAlarms.adapter as AlarmAdapter).swap(it)
+        }
+
+
+        viewModel.currentFilter.observe(viewLifecycleOwner){
+            binding.filterAllAlarms.subFab.isEnabled=true
+            binding.filterWarningAlarms.subFab.isEnabled=true
+            binding.filterCriticalAlarms.subFab.isEnabled=true
+
+            when(it){
+                AlarmsViewModel.Filter.ALL ->       binding.filterAllAlarms.subFab.isEnabled=false
+                AlarmsViewModel.Filter.WARNING ->    binding.filterWarningAlarms.subFab.isEnabled=false
+                AlarmsViewModel.Filter.CRITICAL ->  binding.filterCriticalAlarms.subFab.isEnabled=false
+            }
+        }
+    }
+
+    private fun openFab(){
+        binding.filterAllAlarms.all.visibility = View.VISIBLE
+        binding.filterWarningAlarms.all.visibility = View.VISIBLE
+        binding.filterCriticalAlarms.all.visibility = View.VISIBLE
+
+        binding.filterAllAlarms.all.startAnimation(fromBottom)
+        binding.filterWarningAlarms.all.startAnimation(fromBottom)
+        binding.filterCriticalAlarms.all.startAnimation(fromBottom)
+
+        binding.fabMain.startAnimation(rotateOpen)
+
+        fabOpened=true
+    }
+    private fun closeFab(){
+        binding.filterAllAlarms.all.visibility = View.INVISIBLE
+        binding.filterWarningAlarms.all.visibility = View.INVISIBLE
+        binding.filterCriticalAlarms.all.visibility = View.INVISIBLE
+
+        binding.filterAllAlarms.all.startAnimation(toBottom)
+        binding.filterWarningAlarms.all.startAnimation(toBottom)
+        binding.filterCriticalAlarms.all.startAnimation(toBottom)
+
+        binding.fabMain.startAnimation(rotateClose)
+
+
+        fabOpened=false
     }
 }
--- app/src/main/java/com/carbon7/virtualdisplay/ui/bypass/BypassViewModel.java
+++ app/src/main/java/com/carbon7/virtualdisplay/ui/bypass/BypassViewModel.java
@@ -1,7 +1,8 @@
 package com.carbon7.virtualdisplay.ui.bypass
 
-import androidx.lifecycle.ViewModel
+
 
 class BypassViewModel : ViewModel() {
     //TODO Implement the ViewModel
+        //
 }
