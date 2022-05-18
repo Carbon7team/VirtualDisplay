e9b24151cc21e638ba96b02daffa284353237799
iamadnangazi <adnangazi.ag@gmail.com>
2022-05-18 18:12:03 +0200

    Bypass Fragment complete

--- app/src/main/java/com/carbon7/virtualdisplay/ui/bypass/BypassFragment.kt
+++ app/src/main/java/com/carbon7/virtualdisplay/ui/bypass/BypassFragment.kt
@@ -4,22 +4,15 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
+import androidx.fragment.app.viewModels
 import com.carbon7.virtualdisplay.databinding.FragmentBypassBinding
-import com.carbon7.virtualdisplay.ui.diagram.DiagramFragment
+import com.carbon7.virtualdisplay.model.Measurement
+import com.carbon7.virtualdisplay.ui.UpsDataVisualizerFragment
 
-class BypassFragment : Fragment() {
+class BypassFragment : UpsDataVisualizerFragment() {
+    override val viewModel: BypassViewModel by viewModels()
 
-    companion object {
-        fun newInstance() = DiagramFragment()
-    }
-
-    private lateinit var viewModel: BypassViewModel
     private var _binding: FragmentBypassBinding? = null
-
-    // This property is only valid between onCreateView and
-    // onDestroyView.
     private val binding
         get() = _binding!!
 
@@ -27,11 +20,36 @@ class BypassFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        viewModel = ViewModelProvider(this).get(BypassViewModel::class.java)
-
         _binding = FragmentBypassBinding.inflate(inflater, container, false)
-        val root: View = binding.root
 
-        return root
+        setupView()
+
+        return binding.root
+    }
+
+    private fun searchMeasurementValue(list: List<Measurement>?, code: String) : Float? {
+        if (list != null) {
+            for (measurement in list) {
+                if (measurement.code == code) return measurement.value
+            }
+        }
+        return null
+    }
+
+    private fun setupView(){
+        val measurements = viewModel.filteredMeasurements.value
+        binding.l11.text = searchMeasurementValue(measurements, "==> QUA CI VA L11! <==").toString()
+        binding.l12.text = searchMeasurementValue(measurements, "==> QUA CI VA L12! <==").toString()
+        binding.l13.text = searchMeasurementValue(measurements, "==> QUA CI VA L13! <==").toString()
+        binding.l14.text = searchMeasurementValue(measurements, "==> QUA CI VA L14! <==").toString()
+        binding.l21.text = searchMeasurementValue(measurements, "==> QUA CI VA L21! <==").toString()
+        binding.l22.text = searchMeasurementValue(measurements, "==> QUA CI VA L22! <==").toString()
+        binding.l23.text = searchMeasurementValue(measurements, "==> QUA CI VA L23! <==").toString()
+        binding.l24.text = searchMeasurementValue(measurements, "==> QUA CI VA L24! <==").toString()
+        binding.l31.text = searchMeasurementValue(measurements, "==> QUA CI VA L31! <==").toString()
+        binding.l32.text = searchMeasurementValue(measurements, "==> QUA CI VA L32! <==").toString()
+        binding.l33.text = searchMeasurementValue(measurements, "==> QUA CI VA L33! <==").toString()
+        binding.l34.text = searchMeasurementValue(measurements, "==> QUA CI VA L34! <==").toString()
+        binding.frequency.text = searchMeasurementValue(measurements, "==> QUA CI VA LA FREQUENZA! <==").toString()
     }
 }
