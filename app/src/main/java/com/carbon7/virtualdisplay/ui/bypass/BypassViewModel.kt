77a4ed26c25cb0d03630455e07a40dfb21ea5964
iamadnangazi <adnangazi.ag@gmail.com>
2022-05-18 17:31:11 +0200

    Bypass fragment

--- app/src/main/java/com/carbon7/virtualdisplay/ui/bypass/BypassViewModel.kt
+++ app/src/main/java/com/carbon7/virtualdisplay/ui/bypass/BypassViewModel.kt
@@ -1,8 +1,7 @@
 package com.carbon7.virtualdisplay.ui.bypass
 
-
+import androidx.lifecycle.ViewModel
 
 class BypassViewModel : ViewModel() {
-    //TODO Implement the ViewModel
-        //
+    // TODO: Implement the ViewModel
 }
