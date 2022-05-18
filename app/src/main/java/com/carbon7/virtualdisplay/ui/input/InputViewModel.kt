e6b28dfb330c506690ebe8dc3adba40f3a3dbc5a
iamadnangazi <adnangazi.ag@gmail.com>
2022-05-18 18:18:24 +0200

    Input measurements fragment completed

---
+++ app/src/main/java/com/carbon7/virtualdisplay/ui/input/InputViewModel.kt
@@ -0,0 +1,21 @@
+package com.carbon7.virtualdisplay.ui.bypass
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Transformations
+import com.carbon7.virtualdisplay.model.Measurement
+import com.carbon7.virtualdisplay.ui.UpsDataVisualizerViewModel
+
+class InputViewModel : UpsDataVisualizerViewModel() {
+
+    enum class Filter(val filterFun:(Measurement)->Boolean){
+        ALL({true})
+    }
+
+    private val _currentFilter : MutableLiveData<Filter> = MutableLiveData(Filter.ALL)
+
+    val filteredMeasurements = Transformations.switchMap(measurement){ list ->
+        Transformations.map(_currentFilter){
+            list.filter(it.filterFun)
+        }
+    }
+}
